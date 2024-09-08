f88802ccdde6965e2517a4bd1097bc5497746b64
hazdzz <raphaelpeo@gmail.com>
2024-09-08 16:03:52 +0800

    rename weight in ffn

--- model/converter/ffn.py
+++ model/converter/ffn.py
@@ -11,10 +11,10 @@ class GatedFeedForward(nn.Module):
         assert act_func in ['glu', 'gtu', 'bilinear']
         
         self.length = length
-        self.query_real_linear = nn.Linear(feat_dim, feat_dim, bias=True)
-        self.query_imag_linear = nn.Linear(feat_dim, feat_dim, bias=True)
+        self.value_real_linear = nn.Linear(feat_dim, feat_dim, bias=True)
+        self.value_imag_linear = nn.Linear(feat_dim, feat_dim, bias=True)
         self.key_linear = nn.Linear(feat_dim, feat_dim, bias=True)
-        self.value_linear = nn.Linear(feat_dim, feat_dim, bias=True)
+        self.query_linear = nn.Linear(feat_dim, feat_dim, bias=True)
         self.bffn_dropout = nn.Dropout(p=bffn_drop_prob)
         self.act_func = act_func
         self.softplus = nn.Softplus()
@@ -23,14 +23,14 @@ class GatedFeedForward(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self) -> None:
-        init.xavier_uniform_(self.query_real_linear.weight, gain=1.0)
-        init.xavier_uniform_(self.query_imag_linear.weight, gain=1.0)
+        init.xavier_uniform_(self.value_real_linear.weight, gain=1.0)
+        init.xavier_uniform_(self.value_imag_linear.weight, gain=1.0)
         init.xavier_uniform_(self.key_linear.weight, gain=1.0)
-        init.xavier_uniform_(self.value_linear.weight, gain=1.0)
-        init.zeros_(self.query_real_linear.bias)
-        init.zeros_(self.query_imag_linear.bias)
+        init.xavier_uniform_(self.query_linear.weight, gain=1.0)
+        init.zeros_(self.value_real_linear.bias)
+        init.zeros_(self.value_imag_linear.bias)
         init.zeros_(self.key_linear.bias)
-        init.zeros_(self.value_linear.bias)
+        init.zeros_(self.query_linear.bias)
 
     def forward(self, input: Tensor) -> Tensor:
         if torch.is_complex(input):
@@ -38,30 +38,31 @@ class GatedFeedForward(nn.Module):
         else:
             input_real, input_imag = input, input
         
-        query_real = self.query_real_linear(input_real)
-        query_imag = self.query_imag_linear(input_imag)
+        value_real = self.value_real_linear(input_real)
+        value_imag = self.value_imag_linear(input_imag)
 
         if self.act_func == 'glu':
             # tanh(softplus(x)) is a sigmoid-like function.
-            query = torch.mul(query_real, torch.tanh(self.softplus(query_imag)))
+            value = torch.mul(value_real, torch.tanh(self.softplus(value_imag)))
         elif self.act_func == 'gtu':
             # We replace sigmoid in GTU with softplus.
             # Unlike Dauphin et al. who think tanh hinders back-propagation,
             # we found that sigmoid is what hinders back-propagation.
-            query = torch.mul(torch.tanh(query_real), self.softplus(query_imag))
+            value = torch.mul(torch.tanh(value_real), self.softplus(value_imag))
         else:
-            query = torch.mul(query_real, query_imag)
+            value = torch.mul(value_real, value_imag)
 
-        query = self.bffn_dropout(query)
+        value = self.bffn_dropout(value)
 
         key = self.key_linear(input_real)
-        value = self.value_linear(input_imag)
+        query = self.query_linear(input_imag)
 
         key_norm = key / (torch.norm(key, dim=1, keepdim=True) + 1e-5)
-        value_norm = value / (torch.norm(value, dim=1, keepdim=True) + 1e-5)
+        query_norm = query / (torch.norm(query, dim=1, keepdim=True) + 1e-5)
 
-        kv_attn = torch.einsum('bnd,bne->bde', key_norm, value_norm)
-        kv_attn = self.relu(kv_attn)
-        gffn = torch.einsum('bnd,bde->bne', query, kv_attn)
+        feature_attn = torch.einsum('bnd,bne->bde', key_norm, query_norm)
+        feature_attn = self.relu(feature_attn)
+        
+        gffn = torch.einsum('bnd,bde->bne', value, feature_attn)
 
         return gffn
